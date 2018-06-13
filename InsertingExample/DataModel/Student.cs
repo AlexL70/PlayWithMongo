5d23f31a9b5c6eb8c8a54aec6427096d22547a7f
AlexL70 <alexander.levinson.70@gmail.com>
2018-06-13 22:23:58 +0300

    Simple writing/reading data example

---
+++ InsertingExample/DataModel/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace InsertingExample.DataModel
+{
+    class Student
+    {
+        [Required]
+        //[BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string FirstName { get; set; }
+        [Required]
+        //[BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string LastName { get; set; }
+        [Required]
+        //[BsonRepresentation(MongoDB.Bson.BsonType.Array)]
+        public IEnumerable<string> Subjects { get; set; }
+        //[BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string Class { get; set; }
+        //[BsonRepresentation(MongoDB.Bson.BsonType.Int32)]
+        public int Age { get; set; }
+    }
+}
--- InsertingExample/Program.cs
+++ InsertingExample/Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using InsertingExample.DataModel;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace InsertingExample
 {
     class Program
     {
+        private const string dbName = "school";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().Wait();
+            Console.ReadKey();
+        }
+
+        static async Task MainAsync()
+        {
+            //  Connect to Mongo DB
+            //var mdbClientSettings = new MongoClientSettings
+            //{
+            //    Server = new MongoServerAddress("localhost", 27017),
+            //    UseSsl = false
+            //};
+            //var mdbClient = new MongoClient(mdbClientSettings);
+
+            var mdbConnectionString = "mongodb://localhost:27017";
+            var mdbClient = new MongoClient(mdbConnectionString);
+            IMongoDatabase db = mdbClient.GetDatabase(dbName);
+            //await db.CreateCollectionAsync("students");
+            IMongoCollection<Student> students = db.GetCollection<Student>(nameof(students));
+            var studentCollection = new List<Student>
+            {
+                new Student
+                {
+                    FirstName = "Peter",
+                    LastName = "Gabriel",
+                    Age = 55,
+                    Class = "JSS 3",
+                    Subjects = new List<string> {"English", "Math", "Physics"}
+                },
+                new Student
+                {
+                    FirstName = "Linda",
+                    LastName = "Goodman",
+                    Age = 44,
+                    Class = "PSY 2",
+                    Subjects = new List<string> { "English", "Psychology", "Astrology" }
+                },
+                new Student
+                {
+                    FirstName = "George",
+                    LastName = "Harrison",
+                    Age = 66,
+                    Class = "MUS 1",
+                    Subjects = new List<string> { "Music", "Philosophy", "History of religion" } 
+                }
+            };
+            await students.InsertManyAsync(studentCollection);
+            //await students.InsertOneAsync(new Student
+            //{
+            //    FirstName = "Peter",
+            //    LastName = "Gabriel",
+            //    Age = 55,
+            //    Class = "JSS 3",
+            //    Subjects = new List<string> {"English", "Math", "Physics"}
+            //});
+            var collection = db.GetCollection<BsonDocument>(nameof(students));
+            using (IAsyncCursor<BsonDocument> cursor = await collection.FindAsync(new BsonDocument()))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    IEnumerable<BsonDocument> batch = cursor.Current;
+                    foreach (BsonDocument document in batch)
+                    {
+                        Console.WriteLine(document);
+                        Console.WriteLine();
+                    }
+                }
+            }
+            await db.DropCollectionAsync(nameof(students));
         }
     }
 }
