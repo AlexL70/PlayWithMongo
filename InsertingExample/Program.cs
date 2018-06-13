c34dc7f9ac77261fdee5ac0a1a6a32c59fbe8c8e
AlexL70 <alexander.levinson.70@gmail.com>
2018-06-13 20:53:32 +0300

    Add project files.

---
+++ InsertingExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsertingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
